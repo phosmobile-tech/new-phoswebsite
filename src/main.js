e9b88c715dcf1928059f60dec4d93c739913e1f4
PhosMobile Dell1 <hello@phosmobile.com>
2019-08-20 15:11:10 +0100

    updated files

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 import DynamicLayout from '~/layouts/DynamicLayout.vue'
+import VueDisqus from 'vue-disqus'
+
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
   Vue.component('DynamicLayout', DynamicLayout)
+  Vue.use(VueDisqus)
 }
--- src/pages/Blogs/BlogOne.vue
+++ src/pages/Blogs/BlogOne.vue
@@ -4,6 +4,8 @@
 
       <h1>Post One</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptas? Esse magnam illum aspernatur libero, nihil accusamus ut beatae, exercitationem ipsum temporibus praesentium rem explicabo deserunt obcaecati perspiciatis odio nam?</p>
+
+        <vue-disqus shortname="phosmobile" identifier="$page.post.title"></vue-disqus>
     </Layout>
       
   </div>
